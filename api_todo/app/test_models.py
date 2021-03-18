a1a3be87b3206ec36087777e74b2ad5532f51a8d
vitorrrocha <vitor@gntech.med.br>
2021-03-18 16:42:44 -0300

    Feat: add GET and POST method

---
+++ api_todo/app/test_models.py
@@ -0,0 +1,14 @@
+# from django.test import TestCase
+# from django.db import models
+# from faker import Faker
+# fake = Faker()
+# from .models import Todo
+#
+#
+# def test_todo():
+#     name = fake.name()
+#     done = False
+#     create_at = '28/12/1999'
+#
+#     new_todo = {name, done, create_at}
+#     print(new_todo)
