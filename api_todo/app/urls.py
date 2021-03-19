8df132e2e089b9234ccc4acba555216b963a522b
vitorrrocha <vitor@gntech.med.br>
2021-03-19 13:19:59 -0300

    Feat: add Update and Delete method

--- api_todo/app/urls.py
+++ api_todo/app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import todo_list
+from .views import todo_list, todo_detail_change_and_deleted
 
 urlpatterns = [
     path('', todo_list),
+    path('<int:pk>/', todo_detail_change_and_deleted),
 ]
